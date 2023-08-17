4ab782a9e254dd5fb0c2116a12528cef1d5a25ff
coubs <john.mccoubrie15@gmail.com>
2023-08-17 14:08:44 -0400

    InitialCommit
    Module 1 labs and mortgage calculator challenge lab.

---
+++ ChaptGPTMortgageCalc/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace HomeLoanCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Get Input from the User
+            Console.WriteLine("Enter the purchase price of the home:");
+            double purchasePrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the market value of the home:");
+            double marketValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the down payment amount:");
+            double downPayment = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the interest rate:");
+            double interestRate = double.Parse(Console.ReadLine()) / 100;
+
+            Console.WriteLine("Enter the loan term (15 or 30 years):");
+            int loanTerm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter yearly HOA fees (if any):");
+            double yearlyHOAFees = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the buyer's monthly income:");
+            double monthlyIncome = double.Parse(Console.ReadLine());
+
+            // Step 2: Calculate the Total Loan Amount
+            double originationFee = 0.01 * (purchasePrice - downPayment);
+            double totalLoanValue = purchasePrice - downPayment + originationFee + 2500;
+
+            double equityPercentage = downPayment / marketValue * 100;
+            double loanInsurance = equityPercentage < 10 ? 0.01 * totalLoanValue : 0;
+
+            // Step 3: Calculate the Monthly Payment
+            double monthlyPayment = CalculateMonthlyPayment(totalLoanValue, interestRate, loanTerm);
+            double monthlyHOAFees = yearlyHOAFees / 12;
+            double propertyTax = marketValue * 0.0125 / 12;
+            double homeownersInsurance = marketValue * 0.0075 / 12;
+            double totalMonthlyPayment = monthlyPayment + monthlyHOAFees + propertyTax + homeownersInsurance + loanInsurance / 12;
+
+            // Step 4: Determine Approval or Denial of the Loan
+            if (totalMonthlyPayment >= 0.25 * monthlyIncome)
+            {
+                Console.WriteLine("Loan denied. Consider placing more money down or buying a more affordable home.");
+            }
+            else
+            {
+                Console.WriteLine("Loan approved!");
+            }
+
+            // Step 5: Display the Results
+            Console.WriteLine($"Monthly Payment: ${totalMonthlyPayment:F2}");
+        }
+
+        // Helper method to calculate the monthly payment
+        static double CalculateMonthlyPayment(double principal, double rate, int term)
+        {
+            double r = rate / 12;
+            int n = 12 * term;
+            return principal * (r) * Math.Pow(1 + r, n) / (Math.Pow(1 + r, n) - 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿
+using System.IO;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+struct Employee
+{
+    public string Title { get; set;}
+    public float Price { get; set;}
+
+    public Employee(string title, float price)
+    {
+        Title = title;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        return $"Name: {Title}, Price: {Price}";
+    }
+}
+
+struct MealCombo
+{
+    public string Name { get; }
+    public string MainCourse { get; }
+    public string Side { get; }
+
+    public MealCombo(string name, string mainCourse, string side)
+    {
+        Name = name;
+        MainCourse = mainCourse;
+        Side = side;
+    }
+
+    public override string ToString()
+    {
+        return $"Combo: {Name} {MainCourse} {Side}";
+    }
+}
+
+class Program
+{
+ 
+    //pre defined employees
+    Employee Manager = new Employee("Manager", 30f);
+    Employee Chef = new Employee("Chef", 12f);
+    Employee Server = new Employee("Server", 10f);
+    Employee BusBoy = new Employee("Bus Boy", 8f);
+    Employee Host = new Employee("Host", 5f);
+
+    static MealCombo[] predefinedCombos =
+    {
+        new MealCombo("Disgustingly Healthy", "Salad", "Veggies"),
+        new MealCombo("Heart attackers delight", "Burger", "Fries")
+    };
+
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to John's Deli! Please select a combo or create your own: ");
+        Console.WriteLine("1 - Disgustingly Healthy (Salad & Veggies)");
+        Console.WriteLine("2 - Heart attackers delight (Burger & Fries)");
+        Console.WriteLine("3 - Create your own");
+
+        int comboSelection = Convert.ToInt32(Console.ReadLine());
+
+        MealCombo selectedCombo;
+
+        if(comboSelection >= 1 && comboSelection <= 2)
+        {
+            selectedCombo = predefinedCombos[comboSelection - 1];
+        }
+        else
+        {
+            //Create your own combo
+            string mainResponse = AskForMainCourse();
+            string sideResponse = AskForSide();
+            selectedCombo = new MealCombo("Custom Combo", mainResponse, sideResponse);
+
+            foreach (var combo in predefinedCombos)
+            {
+                if(combo.MainCourse == mainResponse && combo.Side == sideResponse)
+                {
+                    selectedCombo = combo;
+                    break;
+                }
+            }
+        }
+        Console.Write($"Your meal is: {selectedCombo}, enjoy!");
+        
+        static string AskForMainCourse()
+        {
+            Console.WriteLine("Welcome to John's Deli, type the food item you would like: ");
+            Console.WriteLine();
+            Console.WriteLine("Salad");
+            Console.WriteLine();
+            Console.WriteLine("Sandwhich");
+            Console.WriteLine();
+            Console.WriteLine("Burger");
+            Console.WriteLine();
+            Console.WriteLine("Burrito");
+
+            string response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "Salad":
+                    Console.WriteLine("You selected a Salad, what side would you like?");
+                    return response;
+                case "Sandwhich":
+                    Console.WriteLine("You selected a Sandwhich, what side would you like?");
+                    return response;
+                case "Burger":
+                    Console.WriteLine("You selected a Burger, what side would you like?");
+                    return response;
+                case "Burrito":
+                    Console.WriteLine("You selected a Burrito, what side would you like?");
+                    return response;
+                default:
+                    Console.WriteLine("Hmm, that's not on the menu. Please try again!");
+                    return null;
+            }
+        }
+
+        static string AskForSide()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Fries");
+            Console.WriteLine();
+            Console.WriteLine("Drink");
+            Console.WriteLine();
+            Console.WriteLine("Chips");
+            Console.WriteLine();
+            Console.WriteLine("Veggies");
+
+            string response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "Fries":
+                    Console.WriteLine("You selected Fries.");
+                    return response;
+                case "Drink":
+                    Console.WriteLine("You selected a Drink.");
+                    return response;
+                case "Chips":
+                    Console.WriteLine("You selected Chips.");
+                    return response;
+                case "Veggies":
+                    Console.WriteLine("You selected Veggies (ew).");
+                    return response;
+                default:
+                    Console.WriteLine("Hmm, that's not on the menu. Please try again!");
+                    return null;
+            }
+        } 
+        
+    }
+}
